c22e4402f9159747bedd4fac77c022b2a39ff8ee
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2022-05-16 15:58:09 +0500

    Add files via upload

---
+++ inpaint.py
@@ -0,0 +1,41 @@
+import neuralgym as ng
+import numpy as np
+import tensorflow as tf
+
+from generative_inpainting.inpaint_model import InpaintCAModel
+
+
+def inpaint(image, mask, model_dir='manhwa_model'):
+    FLAGS = ng.Config('inpaint.yml')
+    model = InpaintCAModel()
+    assert image.shape[0] == mask.shape[0]
+    assert image.shape[1] == mask.shape[1]
+    h, w, _ = image.shape
+    grid = 8
+    image = image[:h // grid * grid, :w // grid * grid, :]
+    mask = mask[:h // grid * grid, :w // grid * grid, :]
+    print('Shape of image: {}'.format(image.shape))
+    image = np.expand_dims(image, 0)
+    mask = np.expand_dims(mask, 0)
+    input_image = np.concatenate([image, mask], axis=2)
+
+    sess_config = tf.ConfigProto()
+    sess_config.gpu_options.allow_growth = True
+    with tf.Session(config=sess_config) as sess:
+        input_image = tf.constant(input_image, dtype=tf.float32)
+        output = model.build_server_graph(FLAGS, input_image)
+        output = (output + 1.) * 127.5
+        output = tf.reverse(output, [-1])
+        output = tf.saturate_cast(output, tf.uint8)
+        # load pretrained model
+        vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+        assign_ops = []
+        for var in vars_list:
+            vname = var.name
+            from_name = vname
+            var_value = tf.contrib.framework.load_variable(model_dir, from_name)
+            assign_ops.append(tf.assign(var, var_value))
+        sess.run(assign_ops)
+        print('Model loaded.')
+        result = sess.run(output)
+        return result[0][:, :, ::-1]
